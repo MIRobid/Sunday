9753b3ad64289315bad888845fd00f0a60c7bb49
MIRobid <mirobid_i@mail.ru>
2021-12-07 22:12:41 +0500

    Like, dislike and fab added

---
+++ app/src/main/java/com/example/sunday/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.sunday.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is 3rd box");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
